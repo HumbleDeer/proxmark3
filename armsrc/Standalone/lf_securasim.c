2960d7a193d30128ef77bb929ed4afd6a2db770b
HumbleDeer <djsndcx@gmail.com>
2024-12-20 18:21:28 +0100

    modify the pause/button logic & more

--- armsrc/Standalone/lf_securasim.c
+++ armsrc/Standalone/lf_securasim.c
@@ -25,16 +25,17 @@
 #include "util.h"
 #include "dbprint.h"
 
+// Declarations, not worth putting these in an extra header file.
+static void bytes_to_bytebits(const void *src, const size_t srclen, void *dest);
+
+#define raw_hex "7FCC0004436002AF34880000"
+static uint8_t raw_bytes[12] = {0x7F, 0xCC, 0x00, 0x04, 0x43, 0x60, 0x02, 0xAF, 0x34, 0x88, 0x00, 0x00};
+
 void ModInfo(void) {
     DbpString("Secura Key Radio Key (125 kHz) simulator (HumbleDeer)");
 }
 
-void RunMod(void) {
-
-    // 12-byte / 24-bit HEX "raw" data goes here.
-    // err, actually, currently it doesn't do anything at all.
-    // Change your ID/raw code in the "uint8_t raw_bytes[12]" a little further in. ⬇️
-    #define raw_hex = "7FCC0004436002AF34880000"
+void RunMod(void) {    
 
     StandAloneMode();
 
@@ -57,26 +58,22 @@ void RunMod(void) {
             continue;
         }
 
-        LED_C_ON();   // Simulate
+        LED_C_ON();   // Simulation indicator
         LED_D_OFF();
 
-        WAIT_BUTTON_RELEASED();
+        //WAIT_BUTTON_RELEASED();
 
-        // Byte-separated version of the raw HEX code. This converts it into a bytearray. Should be automated/with terminal input in the future.
-        uint8_t raw_bytes[12] = {0x7F, 0xCC, 0x00, 0x04, 0x43, 0x60, 0x02, 0xAF, 0x34, 0x88, 0x00, 0x00}; // The HEX string, but bytes
-        int raw_bytes_length = sizeof(raw_bytes);
-
-        Dbprintf("Simulating SecuraKey - raw " _YELLOW_("%s"), sprint_hex_inrow(raw_bytes, sizeof(raw_bytes)));
+        Dbprintf("Simulating with Radio Key (raw): "_YELLOW_("0x%s"), raw_hex);
 
         uint8_t raw_bits[sizeof(raw_bytes) * 8]; // Make array w/ size of raw[] array, but in discrete bits (8 bits = 1 byte)
         bytes_to_bytebits(raw_bytes, sizeof(raw_bytes), raw_bits); // Place byte array into bit array
 
-        DbpString("[=] Deploying payload...");
+        DbpString("[=] Simulator activated");
         
         // parameters adopted from those in proxmark3/client/src/cmdlfsecurakey.c
-        CmdASKsimTAG((uint8_t) 1, (uint8_t) 0, (uint8_t) 0, (uint8_t) 40, (uint16_t) sizeof(raw_bits), (uint8_t) raw_bits, true);
+        CmdASKsimTAG((uint8_t) 1, (uint8_t) 0, (uint8_t) 0, (uint8_t) 40, (uint16_t) sizeof(raw_bits), raw_bits, true);
 
-        DbpString("[=] Deploying payload finished!");
+        DbpString("[=] Simulator deactivated");
             
     }
 
@@ -84,3 +81,20 @@ void RunMod(void) {
     
     LEDsoff();
 }
+
+static void bytes_to_bytebits(const void *src, const size_t srclen, void *dest) {
+    uint8_t *s = (uint8_t *)src, *d = (uint8_t *)dest;
+    size_t i = srclen * 8, j = srclen;
+
+    while (j--) {
+        uint8_t b = s[j];
+        d[--i] = (b >> 0) & 1;
+        d[--i] = (b >> 1) & 1;
+        d[--i] = (b >> 2) & 1;
+        d[--i] = (b >> 3) & 1;
+        d[--i] = (b >> 4) & 1;
+        d[--i] = (b >> 5) & 1;
+        d[--i] = (b >> 6) & 1;
+        d[--i] = (b >> 7) & 1;
+    }
+}  // 
